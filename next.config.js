1b5688741a87b584fa1b69b8066d5ec5a5e43a55
DevIshola1 <isholad150@gmail.com>
2024-12-11 17:58:28 +0100

    ko

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    output: 'export', // Ensures static export mode
-    basePath: '/req', // Replace <repository-name> with your repo name
-    assetPrefix: 'https://devoliverr.github.io/req/', // Optional: Ensures static assets have the correct prefix
-
-};
